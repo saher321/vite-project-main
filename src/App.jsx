6b8598281e22011e82b616634487822298645bef
saher321 <pnymeet@gmail.com>
2024-12-18 19:58:20 +0500

    code .

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Home from './components/Home'
+import Signup from './forms/Signup';
 
 const App = () => {
   let a = 30;
   return (
     <div className='container'>
-      <Home/>
+ <Signup/>
     </div>
   )
 }
--- src/forms/Signup.jsx
+++ src/forms/Signup.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Signup = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
   return (
-    <div className='form'>
-        <h2>Signup</h2>
+    <div className='form w-25 mx-auto bg-info p-4 rounded mt-5'>
+        <h2 className='text-center my-4'>Signup</h2>
+     <div  className='text-center'>
+     <img src="image/img.jpg" alt="" className='rounded-circle'  style={{width:"80px" , height:"80px"}}/>
+     </div>
         {/* form */}
-        
+        <form className=''>
+       
+       <div className="form-group ">
+        <label htmlFor="name">Name:</label>
+        <input type="text" className="form-control"  id="name" placeholder="Enter name" />
+       </div>
+       <div className="form-group my-3">
+        <label htmlFor="email">Email:</label>
+        <input type="email" className="form-control" id="email" placeholder="Enter email" />
+        </div>
+        <div className="form-group">
+        <label htmlFor="password">Password:</label>
+        <input type="password" className="form-control" id="password" placeholder="Enter password" />
+ 
+       </div>
+       <button type="submit" className="btn btn-primary my-3">Submit</button>
+        </form>
+
       
     </div>
   )
